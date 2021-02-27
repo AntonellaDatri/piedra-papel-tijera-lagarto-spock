9c18fef02ee22cb20914de384247c6d848498da0
Antonella D Atri <antonella.datri@alu.unq.edu.ar>
2021-02-27 19:16:21 -0300

    Updates

---
+++ precache-manifest.fa0fb224161efdfc30a6a6223f0124e2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec724580f147d7c9a37396457a135cfe",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/index.html"
+  },
+  {
+    "revision": "580a6dd2fb796d4992f6",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "02699b9ce272765a5071",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/css/main.ed3d8683.chunk.css"
+  },
+  {
+    "revision": "580a6dd2fb796d4992f6",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/js/2.bbd96415.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/js/2.bbd96415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02699b9ce272765a5071",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/js/main.9ca386f7.chunk.js"
+  },
+  {
+    "revision": "f03c64ed5b95c3ddc99c",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/js/runtime-main.d1cba883.js"
+  },
+  {
+    "revision": "33fcecc23c7b282f189c81d257fd19d0",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/media/Arrow-left-short.33fcecc2.svg"
+  },
+  {
+    "revision": "2787eae132cf2f35433a54468c428dda",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/media/RPSLS.2787eae1.png"
+  },
+  {
+    "revision": "e8609392585200717c7a29761e5d7823",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/media/lagarto.e8609392.png"
+  },
+  {
+    "revision": "5647840a23faf5efa49bf68382ebaaf5",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/media/papel.5647840a.png"
+  },
+  {
+    "revision": "f8e1fb173ff09f10ef885a48292b505e",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/media/piedra.f8e1fb17.png"
+  },
+  {
+    "revision": "188f7143ea2b98ad2094adbcde93be89",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/media/spock.188f7143.png"
+  },
+  {
+    "revision": "988df82dae8526fb389a81ae6bdf9b9d",
+    "url": "/AntonellaDatri/piedra-papel-tijera-lagarto-spock/static/media/tijera.988df82d.png"
+  }
+]);
